ad1a79175caead9a164f7633987bb54534f8c402
Maby <mabigailg@hotmail.com>
2019-05-09 10:33:30 -0600

    Clase Dias no habiles

--- app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea1/DiasNoHabiles.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea1/DiasNoHabiles.java
@@ -1,4 +1,49 @@
 package sv.edu.ues.fia.eisi.grupo06tarea1;
 
-public class DiasNoHabiles {
+
+import java.sql.Date;
+
+
+public class DiasNoHabiles
+{
+    private int idDia;
+    private int ciclo;
+    private Date fecha;
+
+
+    public DiasNoHabiles(){
+
+    }
+
+    public DiasNoHabiles(int idDia, int ciclo, Date fecha) {
+        this.idDia = idDia;
+        this.ciclo = ciclo;
+        this.fecha = fecha;
+    }
+
+
+    public int getIdDia() {
+        return idDia;
+    }
+
+    public void setIdDia(int idDia) {
+        this.idDia = idDia;
+    }
+
+    public int getCiclo() {
+        return ciclo;
+    }
+
+    public void setCiclo(int ciclo) {
+        this.ciclo = ciclo;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
 }
