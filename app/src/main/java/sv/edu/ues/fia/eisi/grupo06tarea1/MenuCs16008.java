56d41f3b4915b154ecbedb78287b00544bbfbfa7
Andres Castro <andres.castro2503@gmail.com>
2019-05-10 17:41:13 -0600

    Agrego activity para propio menu CS16008

---
+++ app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea1/MenuCs16008.java
@@ -0,0 +1,13 @@
+package sv.edu.ues.fia.eisi.grupo06tarea1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuCs16008 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_cs16008);
+    }
+}
