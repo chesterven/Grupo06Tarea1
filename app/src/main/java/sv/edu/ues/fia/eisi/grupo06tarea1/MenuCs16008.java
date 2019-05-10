8d16f96d4ad0bcc38a62edece4e2f3340d76b81f
Andres Castro <andres.castro2503@gmail.com>
2019-05-10 17:54:08 -0600

    Edito activity para propio menu(clase y xml) CS16008

--- app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea1/MenuCs16008.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea1/MenuCs16008.java
@@ -1,7 +1,12 @@
 package sv.edu.ues.fia.eisi.grupo06tarea1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MenuCs16008 extends AppCompatActivity {
 
@@ -10,4 +15,15 @@ public class MenuCs16008 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_cs16008);
     }
+
+    public void solicitudesPrimeraRevision(View v){
+
+    }
+    public void notasEstudianteEvaluacion(View v){
+
+    }
+    public void solicitudImpresion(View v){
+
+    }
+
 }
