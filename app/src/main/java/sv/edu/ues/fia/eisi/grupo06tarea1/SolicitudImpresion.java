2a4ac154ccab78dfdd804450947c0d63b15ce172
Andres Castro <andres.castro2503@gmail.com>
2019-05-11 11:30:10 -0600

    Todo relacionado a la base para Solicitud Impresion  + la modificacion de mis otras tablas, CS16008

---
+++ app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea1/SolicitudImpresion.java
@@ -0,0 +1,71 @@
+package sv.edu.ues.fia.eisi.grupo06tarea1;
+
+public class SolicitudImpresion {
+    private int idSoliImpresion;
+    private String carnet;
+    private String codDocente;
+    private int cantidadExamenes;
+    private int hojasAnexas;
+    private boolean realizada;
+
+    public SolicitudImpresion(int idSoliImpresion,String carnet,String codDocente,int cantidadExamenes,int hojasAnexas, boolean realizada){
+        this.idSoliImpresion=idSoliImpresion;
+        this.carnet=carnet;
+        this.codDocente=codDocente;
+        this.cantidadExamenes=cantidadExamenes;
+        this.hojasAnexas=hojasAnexas;
+        this.realizada=realizada;
+    }
+
+    public int getIdSoliImpresion() {
+        return idSoliImpresion;
+    }
+
+    public void setIdSoliImpresion(int idSoliImpresion) {
+        this.idSoliImpresion = idSoliImpresion;
+    }
+
+    public String getCarnet() {
+        return carnet;
+    }
+
+    public void setCarnet(String carnet) {
+        this.carnet = carnet;
+    }
+
+    public String getCodDocente() {
+        return codDocente;
+    }
+
+    public void setCodDocente(String codDocente) {
+        this.codDocente = codDocente;
+    }
+
+    public int getCantidadExamenes() {
+        return cantidadExamenes;
+    }
+
+    public void setCantidadExamenes(int cantidadExamenes) {
+        this.cantidadExamenes = cantidadExamenes;
+    }
+
+    public int getHojasAnexas() {
+        return hojasAnexas;
+    }
+
+    public void setHojasAnexas(int hojasAnexas) {
+        this.hojasAnexas = hojasAnexas;
+    }
+
+    public boolean isRealizada() {
+        return realizada;
+    }
+
+    public void setRealizada(boolean realizada) {
+        this.realizada = realizada;
+    }
+
+
+
+
+}
