e37e05990dc6e3bf18e397fccd78f3939edf0e29
Maby <mabigailg@hotmail.com>
2019-05-10 20:20:13 -0600

    Arreglando error de la activity del detalle segunda revision

---
+++ app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea1/DetalleSegundaRevision_Actualizar.java
@@ -0,0 +1,13 @@
+package sv.edu.ues.fia.eisi.grupo06tarea1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetalleSegundaRevision_Actualizar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_segunda_revision__actualizar);
+    }
+}
