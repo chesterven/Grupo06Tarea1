10f4a55725aebcdca04d9ed015f671eff16e48f8
Andres Castro <andres.castro2503@gmail.com>
2019-05-10 19:06:32 -0600

    Agrego clase y xml relacionados con la insercion para NotasEstudianteEvaluacion

---
+++ app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea1/NotasEstudianteEvaluacion_Insertar.java
@@ -0,0 +1,15 @@
+//CS16008 Castro Sánchez José Andrés
+
+package sv.edu.ues.fia.eisi.grupo06tarea1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotasEstudianteEvaluacion_Insertar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notas_estudiante_evaluacion__insertar);
+    }
+}
