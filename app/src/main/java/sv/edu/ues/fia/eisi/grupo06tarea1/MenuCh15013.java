e2db5e2c1f23a3291313561c59abb09970870fc9
Oscar Cordero <oscarcordero97@gmail.com>
2019-05-12 20:48:46 -0600

    oscar commit's 3

--- app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea1/MenuCh15013.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea1/MenuCh15013.java
@@ -7,6 +7,9 @@ package sv.edu.ues.fia.eisi.grupo06tarea1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
 public class MenuCh15013 extends AppCompatActivity {
@@ -23,5 +26,56 @@ public class MenuCh15013 extends AppCompatActivity {
         DBHelper.abrir();
         arrayAccesos = DBHelper.consultarAccesos(idusuario);
         DBHelper.cerrar();
+    }
+    public void solicitudSegundaRevision(View v){
+        String codOpcion = "005";
+        boolean permitido = false;
+        for(int i=0; i<arrayAccesos.size();i++){
+            if(codOpcion.equals(arrayAccesos.get(i))){
+                permitido = true;
+            }
+        }
+        if(permitido || idusuario.equals("01")){
+            Toast.makeText(this,"Tiene Acceso", Toast.LENGTH_SHORT).show();
+            Intent ven=new Intent(this,MenuSolicitudSegundaRevision.class);
+            startActivity(ven);
+        }
+        else{
+            Toast.makeText(this,"No Tiene Acceso", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    public void primeraRevision(View v){
+        String codOpcion = "007";
+        boolean permitido = false;
+        for(int i=0; i<arrayAccesos.size();i++){
+            if(codOpcion.equals(arrayAccesos.get(i))){
+                permitido = true;
+            }
+        }
+        if(permitido || idusuario.equals("01")){
+            Toast.makeText(this,"Tiene Acceso", Toast.LENGTH_SHORT).show();
+            Intent ven=new Intent(this,MenuPrimeraRevision.class);
+            startActivity(ven);
+        }
+        else{
+            Toast.makeText(this,"No Tiene Acceso", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void detallePrimeraRevisión(View v) {
+        String codOpcion = "011";
+        boolean permitido = false;
+        for (int i = 0; i < arrayAccesos.size(); i++) {
+            if (codOpcion.equals(arrayAccesos.get(i))) {
+                permitido = true;
+            }
+        }
+        if (permitido || idusuario.equals("01")) {
+            Toast.makeText(this, "Tiene Acceso", Toast.LENGTH_SHORT).show();
+            Intent ven = new Intent(this, MenuDetallePrimeraRevision.class);
+            startActivity(ven);
+        } else {
+            Toast.makeText(this, "No Tiene Acceso", Toast.LENGTH_SHORT).show();
+        }
     }
 }
