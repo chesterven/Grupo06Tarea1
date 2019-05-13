9012453caeb4b4f6762cd8788853fff072bcf193
Oscar Cordero <oscarcordero97@gmail.com>
2019-05-13 00:36:16 -0600

    oscar commit's 4

--- app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea1/MenuSolicitudSegundaRevision.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea1/MenuSolicitudSegundaRevision.java
@@ -1,7 +1,9 @@
 package sv.edu.ues.fia.eisi.grupo06tarea1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuSolicitudSegundaRevision extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MenuSolicitudSegundaRevision extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_solicitud_segunda_revision);
+    }
+
+    public void insertarSolicitudSegundaRevision(View v){
+        Intent inte=new Intent(this,SolicitudSegundaRevision_Insertar.class);
+        startActivity(inte);
+    }
+
+    public void consultarSolicitudSegundaRevision(View v){
+        Intent inte=new Intent(this,SolicitudSegundaRevision_Consultar.class);
+        startActivity(inte);
+    }
+
+    public void actualizarSolicitudSegundaRevision(View v){
+        Intent inte=new Intent(this,SolicitudSegundaRevision_Actualizar.class);
+        startActivity(inte);
+    }
+
+    public void eliminarSolicitudSegundaRevision(View v){
+        Intent inte=new Intent(this,SolicitudSegundaRevision_Eliminar.class);
+        startActivity(inte);
     }
 }
---
+++ app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea1/SolicitudSegundaRevision_Eliminar.java
@@ -0,0 +1,13 @@
+package sv.edu.ues.fia.eisi.grupo06tarea1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SolicitudSegundaRevision_Eliminar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solicitud_segunda_revision__eliminar);
+    }
+}
