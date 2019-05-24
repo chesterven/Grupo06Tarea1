2858710be61caa0804d2d818f665821b60b7a9c5
Oscar Cordero <oscarcordero97@gmail.com>
2019-05-23 21:44:24 -0600

    strings detalle primera revision

--- app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea1/MenuPrimeraRevision.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea1/MenuPrimeraRevision.java
@@ -1,17 +1,108 @@
 package sv.edu.ues.fia.eisi.grupo06tarea1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MenuPrimeraRevision extends AppCompatActivity {
+    String idusuario = "";
+    DBHelperInicial DBHelper;
+    ArrayList<String> arrayAccesos = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_primera_revision);
+
+        Intent recibir = getIntent();
+        idusuario = recibir.getStringExtra("idusuario");
+
+        DBHelper = new DBHelperInicial(this);
+        DBHelper.abrir();
+        arrayAccesos = DBHelper.consultarAccesos(idusuario);
+        DBHelper.cerrar();
     }
 
-    public void hola(View v){
+    public void segundaRevisionInsertar(View v){
+
+        String codOpcion = "036";
+        boolean permitido = false;
+        for(int i=0; i<arrayAccesos.size();i++){
+            if(codOpcion.equals(arrayAccesos.get(i))){
+                permitido = true;
+            }
+        }
+        if(permitido || idusuario.equals("01")){
+            Toast.makeText(this,"Tiene Acceso", Toast.LENGTH_SHORT).show();
+            Intent ven=new Intent(this,SegundaRevision_Insertar.class);
+            startActivity(ven);
+        }
+        else{
+            Toast.makeText(this,"No Tiene Acceso", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void segundaRevisionEliminar(View v){
+
+        String codOpcion = "039";
+        boolean permitido = false;
+        for(int i=0; i<arrayAccesos.size();i++){
+            if(codOpcion.equals(arrayAccesos.get(i))){
+                permitido = true;
+            }
+        }
+        if(permitido || idusuario.equals("01")){
+            Toast.makeText(this,"Tiene Acceso", Toast.LENGTH_SHORT).show();
+            Intent ven=new Intent(this,SegundaRevision_Eliminar.class);
+            startActivity(ven);
+        }
+        else{
+            Toast.makeText(this,"No Tiene Acceso", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void segundaRevisionActualizar(View v){
+        String codOpcion = "037";
+        boolean permitido = false;
+        for(int i=0; i<arrayAccesos.size();i++){
+            if(codOpcion.equals(arrayAccesos.get(i))){
+                permitido = true;
+            }
+        }
+        if(permitido || idusuario.equals("01")){
+            Toast.makeText(this,"Tiene Acceso", Toast.LENGTH_SHORT).show();
+            Intent ven=new Intent(this,SegundaRevision_Actualizar.class);
+            startActivity(ven);
+        }
+        else{
+            Toast.makeText(this,"No Tiene Acceso", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void segundaRevisionConsultar(View v){
+        String codOpcion = "038";
+        boolean permitido = false;
+        for(int i=0; i<arrayAccesos.size();i++){
+            if(codOpcion.equals(arrayAccesos.get(i))){
+                permitido = true;
+            }
+        }
+        if(permitido || idusuario.equals("01")){
+            Toast.makeText(this,"Tiene Acceso", Toast.LENGTH_SHORT).show();
+            Intent ven=new Intent(this,SegundaRevision_Consultar.class);
+            startActivity(ven);
+        }
+        else{
+            Toast.makeText(this,"No Tiene Acceso", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 }
--- app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea1/PrimeraRevision_Insertar.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/grupo06tarea1/PrimeraRevision_Insertar.java
@@ -1,13 +1,102 @@
 package sv.edu.ues.fia.eisi.grupo06tarea1;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class PrimeraRevision_Insertar extends AppCompatActivity {
+    DBHelperInicial DBHelper;
+    EditText codDocente;
+    Spinner locales;
+    Spinner evalua;
+    EditText fecha;
+    EditText descripcion;
+    EditText hora;
+    ArrayList<String> localess=new ArrayList<>();
+    ArrayList<String> evaluaciones=new ArrayList<>();
+    Cursor resultadosEvaluaciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_primera_revision__insertar);
+        codDocente = (EditText)findViewById(R.id.idCodigoDocenteConsultarPrimera);
+        locales = (Spinner) findViewById(R.id.spinnerLocales);
+        evalua= (Spinner) findViewById(R.id.spinnerEvaluacionesDocentePrimera);
+        fecha = (EditText) findViewById(R.id.fechaPrimeraRevisionIns);
+        hora = (EditText) findViewById(R.id.horaPRimeraRevisionIns);
+        descripcion = (EditText) findViewById(R.id.descripcionPrimeraRevisionIns);
+        localess.add("Seleccione el local");
+        DBHelper = new DBHelperInicial(this);
+        DBHelper.abrir();
+        Cursor resultado=DBHelper.consultarLocales();
+        if(resultado.moveToFirst())
+        {
+            do {
+                localess.add(String.valueOf(resultado.getInt(0)) + " " + resultado.getString(1));
+            }while(resultado.moveToNext());
+        }
+
+
+        ArrayAdapter<CharSequence> adaptador = new ArrayAdapter(this,android.R.layout.simple_spinner_item,localess);
+        locales.setAdapter(adaptador);
+    }
+
+    public  void consultarEvaPrimeraRevi(View v)
+    {
+        if(!(codDocente.getText().toString().equals(""))) {
+            DBHelper = new DBHelperInicial(this);
+            DBHelper.abrir();
+
+            Cursor materiaCiclo = DBHelper.consultarMateriasDocente(codDocente.getText().toString());
+
+            if (materiaCiclo.moveToFirst()) {
+                do {
+                    resultadosEvaluaciones=DBHelper.consultarEvaluaciones(materiaCiclo.getInt(0), materiaCiclo.getString(1), materiaCiclo.getInt(2));
+
+                    if(resultadosEvaluaciones.moveToFirst())
+                    {
+                        do {
+                            evaluaciones.add(resultadosEvaluaciones.getInt(0)+ " "+resultadosEvaluaciones.getString(1)+" "+resultadosEvaluaciones.getString(2));
+
+                        }while(resultadosEvaluaciones.moveToNext());
+                    }
+                    else{
+
+
+
+                    }
+                } while (materiaCiclo.moveToNext());
+
+                if(evaluaciones.size()==0)
+                {
+                    Toast.makeText(this, "No hay evaluaciones para ese docente", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    evaluaciones.add(0,"Seleccione su evaluación");
+                    ArrayAdapter<CharSequence> adaptadorr = new ArrayAdapter(this, android.R.layout.simple_spinner_item, evaluaciones);
+                    evalua.setAdapter(adaptadorr);
+                    Toast.makeText(this, "Evaluaciones encontradas", Toast.LENGTH_SHORT).show();
+                }
+            }
+            else{
+                Toast.makeText(this, "No existe el código del docente", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+
+        else
+        {
+            Toast.makeText(this,"Ingrese el código del docente", Toast.LENGTH_SHORT).show();
+        }
     }
 }
